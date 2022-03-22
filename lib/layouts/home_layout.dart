2c1b5136e8e4f1d9fab19cad0e1e4b0a60b8ea3c
jabirelamean <jabirelamean3@gmail.com>
2022-03-22 11:57:31 +0300

    creating database using sqflite

--- lib/layouts/home_layout.dart
+++ lib/layouts/home_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app1/modules/archieved_tasks/archived_tasks_screen.dart';
-
+import 'package:sqflite/sqflite.dart';
 import '../modules/done_tasks/done_tasks_screen.dart';
 import '../modules/new_tasks/new_tasks_screen.dart';
 
@@ -10,6 +10,14 @@ class HomeLayout extends StatefulWidget {
   @override
   State<HomeLayout> createState() => _HomeLayoutState();
 }
+
+// 1- create database
+// 2- create tables
+// 2- open database
+// 3- insert to database
+// 4- get from database
+// 5- update in database
+// 6- delete from database
 
 class _HomeLayoutState extends State<HomeLayout> {
   int selectedIndex = 0;
@@ -26,14 +34,22 @@ class _HomeLayoutState extends State<HomeLayout> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    createDatabase();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${titles[selectedIndex]}'),
+        title: Text(titles[selectedIndex]),
       ),
       body: screens[selectedIndex],
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {
+
+        },
         child: Icon(Icons.add),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -59,6 +75,27 @@ class _HomeLayoutState extends State<HomeLayout> {
           ),
         ],
       ),
+    );
+  }
+
+  void createDatabase() async{
+    var database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version){
+        print('db created');
+        //creating the table id,title,date, time, status
+        database.execute(
+          'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)'
+        ).then((value){
+          print('table created');
+        }).catchError((error){
+          print('Error while creating table ${error.toString()}');
+        });
+      },
+      onOpen: (database){
+        print('db opened');
+      }
     );
   }
 }
