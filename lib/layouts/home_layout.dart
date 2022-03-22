2b9b5b575db5d81fd11d324e56ac312ce70860b3
jabirelamean <jabirelamean3@gmail.com>
2022-03-22 16:38:41 +0300

    setting a time picker

--- lib/layouts/home_layout.dart
+++ lib/layouts/home_layout.dart
@@ -36,6 +36,8 @@ class _HomeLayoutState extends State<HomeLayout> {
   Database? database;
   var scaffoldKey = GlobalKey<ScaffoldState>();
   bool isBottomSheetOpen = false;
+  var titleController = TextEditingController();
+  var timeController = TextEditingController();
 
   @override
   void initState() {
@@ -54,74 +56,126 @@ class _HomeLayoutState extends State<HomeLayout> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            if(isBottomSheetOpen) {
+            if (isBottomSheetOpen) {
               Navigator.pop(context);
               isBottomSheetOpen = false;
             } else {
               scaffoldKey.currentState?.showBottomSheet((context) => Container(
-                width: double.infinity,
-                height: 120,
-                color: Colors.blue,
-              ));
+                    color: Colors.grey[200],
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //title textField
+                        TextFormField(
+                          controller: titleController,
+                          keyboardType: TextInputType.text,
+                          validator: (value) {
+                            if (value == null) {
+                              return 'title must not be empty';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: Text('Task title'),
+                            prefix: Icon(
+                              Icons.title,
+                              color: Colors.grey,
+                            ),
+                            enabledBorder: new OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 1.0)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 14,
+                        ),
+                        //time textField
+                        TextFormField(
+                          controller: timeController,
+                          keyboardType: TextInputType.datetime,
+                          onTap: () async{
+                            await showTimePicker (
+                              context: context,
+                              initialTime: TimeOfDay.now(),
+                            ).then((value) {
+                              if(value == null) {
+                                return null;
+                              } else {
+                                timeController.text = value.format(context);
+                              }
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null) {
+                              return 'time must not be empty';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: Text('Task Time'),
+                            prefix: Icon(
+                              Icons.watch_later_outlined,
+                              color: Colors.grey,
+                            ),
+                            enabledBorder: new OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 1.0)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ));
               isBottomSheetOpen = true;
             }
           });
-
         },
-        child: Icon(isBottomSheetOpen?Icons.add:Icons.edit),
+        child: Icon(isBottomSheetOpen ? Icons.add : Icons.edit),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: selectedIndex,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             selectedIndex = index;
           });
         },
         items: [
+          BottomNavigationBarItem(label: 'Tasks', icon: Icon(Icons.menu)),
+          BottomNavigationBarItem(label: 'Done', icon: Icon(Icons.check)),
           BottomNavigationBarItem(
-            label: 'Tasks',
-            icon: Icon(Icons.menu)
-          ),
-          BottomNavigationBarItem(
-            label: 'Done',
-              icon: Icon(Icons.check)
-          ),
-          BottomNavigationBarItem(
-            label: 'Archived',
-              icon: Icon(Icons.archive_outlined)
-          ),
+              label: 'Archived', icon: Icon(Icons.archive_outlined)),
         ],
       ),
     );
   }
 
-  void createDatabase() async{
-    database = await openDatabase(
-      'todo.db',
-      version: 1,
-      onCreate: (database, version){
-        print('db created');
-        //creating the table id,title,date, time, status
-        database.execute(
-          'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)'
-        ).then((value){
-          print('table created');
-        }).catchError((error){
-          print('Error while creating table ${error.toString()}');
-        });
-      },
-      onOpen: (database){
-        print('db opened');
-      }
-    );
+  void createDatabase() async {
+    database = await openDatabase('todo.db', version: 1,
+        onCreate: (database, version) {
+      print('db created');
+      //creating the table id,title,date, time, status
+      database
+          .execute(
+              'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
+          .then((value) {
+        print('table created');
+      }).catchError((error) {
+        print('Error while creating table ${error.toString()}');
+      });
+    }, onOpen: (database) {
+      print('db opened');
+    });
   }
 
-  void insertToDatabase(){
-    database?.transaction((txn) async{
-      txn.rawInsert('INSERT INTO tasks(title, date, time, status) VALUES("first task", "03322", "123", "new")').then((value) {
+  void insertToDatabase() {
+    database?.transaction((txn) async {
+      txn
+          .rawInsert(
+              'INSERT INTO tasks(title, date, time, status) VALUES("first task", "03322", "123", "new")')
+          .then((value) {
         print('$value inserted succesfuly}');
-      }).catchError((error){
+      }).catchError((error) {
         print('Error while inserting new record ${error.toString()}');
       });
     });
